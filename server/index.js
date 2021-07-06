ccbc25820780f67ecb049f52d98badaa100122ab
Linh Hoang <Linh.Hoang@devmounta.in>
2021-07-06 15:56:54 -0500

    updated heroku

--- server/index.js
+++ server/index.js
@@ -19,6 +19,12 @@ app.get('/', function(req,res){
     res.sendFile(path.join(__dirname, '../index.html'))
    
 })
+app.get('/styles', function(req, res) {
+    res.sendFile( path.join(__dirname, '/styles.css') );
+})
+app.get('/js', function(req, res) {
+    res.sendFile( path.join(__dirname, '/main.js') )
+})
 
 const port = process.env.PORT || 5050
 
